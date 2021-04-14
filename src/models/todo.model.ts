67d9ef5275a60eca5f55d8810e365285c4473237
Tiagomgs <tiagomiguelgs@hotmail.com>
2021-04-14 15:08:30 +0100

    Add files via upload

---
+++ src/models/todo.model.ts
@@ -0,0 +1,27 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  title?: string;
+
+  @property({
+    type: 'boolean',
+  })
+  isComplete?: boolean;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
